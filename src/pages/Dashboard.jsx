ba8da5d0f3860963b7706aca9c4d4710ad316adc
abubakar308 <mdabubakarsiddique789@gmail.com>
2025-08-05 22:12:58 +0600

    OurMenu page Added

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,14 @@
+import { Link } from "react-router-dom";
 
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div>
+      <h1>Dashboard</h1>
+      <p>Welcome to your dashboard!</p>
+<aside>
+  <li><Link to="#account-settings">Account Settings</Link></li>
+  <li><Link to="#activity-log">Activity Log</Link></li>
+</aside>
+    </div>
   )
 }
